ca46483c83fd5e68564126e1d8aead574ee379a3
itair <itair@163.com>
2013-12-23 19:43:55 +0800

    第三题

--- Try12231/try.cpp
+++ Try12231/try.cpp
@@ -29,27 +29,48 @@ void main(){
 	//
 
 	//2
- 	int stature,weight;
+//  	int stature,weight;
+// 
+//  	const int inchesprefoor=12;
+//  	cout<< " Enter your stature by inches: _______\b\b\b\b\b";
+//  	cin>> stature;
+// 	cout<< " Enter your bodyweight by pounds: _______\b\b\b\b\b";
+// 	cin>>weight;
+// 
+// 	const double metersperinch = 0.0254;
+// 	const double poundsperkg = 2.2;
+//  	int hf,hi; 	
+// 	double kg,bmi,meter;
+// 	hf = stature / inchesprefoor;
+// 	hi = stature % inchesprefoor;
+// 	meter = stature * metersperinch;
+// 	kg = weight / poundsperkg;
+// 	bmi = kg / ( meter * meter);
+// 	cout<<" Your body statics are "<<meter<<"m and "<<kg<<" kg"<<endl;
+// 	cout<< " Your BMI is: "<<bmi<<endl;
+// 	cout<<"_________________________________"<<endl<<endl;   
+// 	// ¼ÆËã
 
- 	const int inchesprefoor=12;
- 	cout<< " Enter your stature by inches: _______\b\b\b\b\b";
- 	cin>> stature;
-	cout<< " Enter your bodyweight by pounds: _______\b\b\b\b\b";
-	cin>>weight;
+	//3
+	int de,mi,se;
+	double deg;
+	const int miperde = 60;
+	const int sepermi = 60;
 
-	const double metersperinch = 0.0254;
-	const double poundsperkg = 2.2;
- 	int hf,hi; 	
-	double kg,bmi,meter;
-	hf = stature / inchesprefoor;
-	hi = stature % inchesprefoor;
-	meter = stature * metersperinch;
-	kg = weight / poundsperkg;
-	bmi = kg / ( meter * meter);
-	cout<<" Your body statics are "<<meter<<"m and "<<kg<<" kg"<<endl;
-	cout<< " Your BMI is: "<<bmi<<endl;
+	cout<<"Ener a latitude in degrees, minutes, and seconds:\n"
+			<<"First, enter the degrees:";
+	cin>>de;
+	cout<<"Next, enter the minutes of arc:";
+	cin >>mi;
+	cout<<"Finally , enter the second of arc:";
+	cin >>se;
+	deg = de + 1.0/miperde * mi + 1.0/ miperde * 1.0/sepermi * se;
+	
+	cout<<de<<" degrees, "
+		<<mi<<" minutes, "
+		<<se<<" seconds = "
+		<<deg<<"degrees\n";
 	cout<<"_________________________________"<<endl<<endl;   
-	//¼ÆËã×ª»»£»
 
 	system("pause");
 
