a029bc634324e12db62af2f082c6c24298e6f2d5
itair <itair@163.com>
2013-12-23 20:57:57 +0800

    第五第六题

--- Try12231/try.cpp
+++ Try12231/try.cpp
@@ -25,7 +25,7 @@ void main(){
 // 	hf = height / inchesprefoor;
 // 	hi = height % inchesprefoor;
 //   cout<< "\n Your height is "<< hf <<" feet and "<< hi <<" inches. "<<endl;
-// 	cout<<"_________________________________"<<endl<<endl;   
+ 	cout<<"_________________________________"<<endl<<endl;   
 	//
 
 	//2
@@ -48,7 +48,7 @@ void main(){
 // 	bmi = kg / ( meter * meter);
 // 	cout<<" Your body statics are "<<meter<<"m and "<<kg<<" kg"<<endl;
 // 	cout<< " Your BMI is: "<<bmi<<endl;
-// 	cout<<"_________________________________"<<endl<<endl;   
+ 	cout<<"_________________________________"<<endl<<endl;   
 // 	// ¼ÆËã
 
 	//3
@@ -70,27 +70,43 @@ void main(){
 // 		<<mi<<" minutes, "
 // 		<<se<<" seconds = "
 // 		<<deg<<"degrees\n";
-// 	cout<<"_________________________________"<<endl<<endl;   
-
+ 	cout<<"_________________________________"<<endl<<endl; 
 	//4
-	long allseconds,temp;
-	int days,hours,minutes,seconds;
-	const int kHoursInADay=24;
-	const int kMinutesInAnHour=60;
-	const int kSecondsInAMinute=60;
-	cout<<"Enter a long number of seconds: "<<endl;
-	cin>>allseconds;	
-	seconds = allseconds % kSecondsInAMinute;
-	temp		= allseconds / kSecondsInAMinute;
-	minutes = temp % kMinutesInAnHour;
-	temp		= temp / kMinutesInAnHour;
-	hours		= temp % kHoursInADay;
-	days		= temp / kHoursInADay;
-	printf("%d seconds = %d days, %d minutes , %d seconds\n", 
-					allseconds, days, minutes, seconds);
+// 	long allseconds,temp;
+// 	int days,hours,minutes,seconds;
+// 	const int kHoursInADay=24;
+// 	const int kMinutesInAnHour=60;
+// 	const int kSecondsInAMinute=60;
+// 	cout<<"Enter a long number of seconds: "<<endl;
+// 	cin>>allseconds;	
+// 	seconds = allseconds % kSecondsInAMinute;
+// 	temp		= allseconds / kSecondsInAMinute;
+// 	minutes = temp % kMinutesInAnHour;
+// 	temp		= temp / kMinutesInAnHour;
+// 	hours		= temp % kHoursInADay;
+// 	days		= temp / kHoursInADay;
+// 	printf("%d seconds = %d days, %d minutes , %d seconds\n", 
+// 					allseconds, days, minutes, seconds);
 	cout<<"_________________________________\n\n"; 
-
-
+	//5
+	double range, petrol, fuelrate;
+	const int k100KM=100;
+	cout<<"Enter the km of distance your had driven:\n";
+	cin>>range;
+	cout<<"Enter the liter of petrol the car used:\n";
+	cin>>petrol;
+	fuelrate = petrol / range * k100KM;
+	printf("Your car's fuel rate is %f L/100km\n", fuelrate);
+	cout<<"_________________________________\n\n"; 
+	//6
+	double range_mile, petrol_gallon, fuelrate_mpg;
+	const double kMilesPer100km = 62.14;
+	const double kLitersPerGallon = 3.875;
+	range_mile = range / k100KM * kMilesPer100km;
+	petrol_gallon = petrol / kLitersPerGallon;
+	fuelrate_mpg =  range_mile / petrol_gallon;
+	cout<< "Your 'US' Car can run "<<fuelrate_mpg<< " Miles/Gallon.\n";
+	cout<<"_________________________________\n\n"; 
 
 
 	system("pause");
